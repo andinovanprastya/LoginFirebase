6861bffc0e37993c6ae933efbc72ff570753fe79
aurakanzaa <aurakanzaa@yahoo.co.id>
2018-10-22 00:35:00 +0700

    SIGN OUT

--- app/src/main/java/com/example/andinovanprastya/loginfirebase/fragment/SignoutFragment.java
+++ app/src/main/java/com/example/andinovanprastya/loginfirebase/fragment/SignoutFragment.java
@@ -1,9 +1,12 @@
 package com.example.andinovanprastya.loginfirebase.fragment;
 
 import android.content.Context;
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,30 +16,21 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.andinovanprastya.loginfirebase.R;
+import com.example.andinovanprastya.loginfirebase.login.LoginActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 
 public class SignoutFragment extends Fragment {
 
-    private FirebaseAuth mAuth;
+    // firebase auth
+    private FirebaseAuth.AuthStateListener mAuth;
+
+    // button
     private Button mButton;
 
     private static final String TAG = "EmailPassword";
 
-
-    private OnFragmentInteractionListener mListener;
-    private View.OnClickListener mOnClickListener = new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            int i = v.getId();
-            switch (i){
-                case R.id.signOutButton2:
-                    signOut();
-                    break;
-            }
-        }
-    };
 
     public SignoutFragment() {
         // Required empty public constructor
@@ -51,9 +45,41 @@ public class SignoutFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mAuth = FirebaseAuth.getInstance();
-        setHasOptionsMenu(true);
 
+    }
+
+    private void setupFirebaseListener(){
+        Log.d(TAG, "setupFirebaseListener: settong up the auth state listener");
+        mAuth = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if(user != null){
+                    Log.d(TAG,"auth sign in: "+user.getUid() );
+                }else {
+                    Log.d(TAG,"auth sign out" );
+                    Toast.makeText(getActivity(),"sign out" , Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent(getActivity(), LoginActivity.class);
+                    intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                    startActivity(intent);
+                }
+            }
+        };
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        FirebaseAuth.getInstance().addAuthStateListener(mAuth);
+    }
+
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if(mAuth != null){
+            FirebaseAuth.getInstance().removeAuthStateListener(mAuth);
+        }
     }
 
     @Override
@@ -63,39 +89,26 @@ public class SignoutFragment extends Fragment {
 
         View view = inflater.inflate(R.layout.fragment_signout, container, false);
         mButton = view.findViewById(R.id.signOutButton2);
-        mButton.setOnClickListener(mOnClickListener);
+
+        setupFirebaseListener();
+
+        mButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(TAG, "onClick: attempting to sign out the user");
+                FirebaseAuth.getInstance().signOut();
+            }
+        });
 
         return view;
     }
 
 
 
-    private void initView(View view) {
-        mButton = view.findViewById(R.id.signOutButton2);
-    }
-
-
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-//            throw new RuntimeException(context.toString()
-//                    + " must implement OnFragmentInteractionListener");
-        }
-    }
-
     @Override
     public void onDetach() {
         super.onDetach();
-        mListener = null;
+
     }
 
 
@@ -108,11 +121,11 @@ public class SignoutFragment extends Fragment {
 //        mAuth.signOut();
 //    }
 
-    private void signOut(){
-        if(mAuth != null){
-            mAuth.signOut();
-        }
-    }
+//    private void signOut(){
+//        if(mAuth != null){
+//            mAuth.signOut();
+//        }
+//    }
 
 
 
